85a4615238f41cd0ed97b76d2d6cd89e21be3fbc
sserranovar <125494152+sserranovar@users.noreply.github.com>
2023-09-15 20:05:00 +0000

    565

--- main.py
+++ main.py
@@ -38,3 +38,4 @@ print(presencebeach)
 # Check the Python Documentation to find the description of the len() function. Then, display on the screen the length (in number of characters) of the word electroencephalographist.
 bigwrd="electroencephalographist"
 print(len(bigwrd))
+print("test")
